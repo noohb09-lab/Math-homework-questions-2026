06e168525e8e85873c172264855075b91e3a4373
noohb09-lab <noohb09@gmail.com>
2026-02-19 11:08:15 +1030

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-onst io = require('socket.io')(3000, { cors: { origin: "*" } });
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: { origin: "*" }
+});
+
 let players = {};
 
-console.log("Server is starting...");
+// This part tells Render the server is "Healthy"
+app.get('/', (req, res) => {
+  res.send('Server is Running');
+});
 
 io.on('connection', (socket) => {
-  // Give every new player a random position and color
   players[socket.id] = {
     x: Math.random() * 20 - 10,
     z: Math.random() * 20 - 10,
     color: Math.random() * 0xffffff
   };
 
-  // Send the list of players to the person who just joined
   socket.emit('currentPlayers', players);
-  // Tell everyone else a new player is here
   socket.broadcast.emit('newPlayer', { id: socket.id, info: players[socket.id] });
 
-  // When someone moves, tell everyone else
   socket.on('move', (pos) => {
     if (players[socket.id]) {
       players[socket.id].x = pos.x;
@@ -29,3 +34,10 @@ io.on('connection', (socket) => {
     delete players[socket.id];
     io.emit('playerDisconnected', socket.id);
   });
+});
+
+// CRITICAL FIX: Render assigns a random port, this code finds it
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log('Server is live on port ' + PORT);
+});
